129f3d93f8dcdc8897a322c91ccbed26d3df7718
Null-Pointer <afolanyaaminu@gmail.com>
2016-06-27 04:03:04 +0000

    rename socket_io_client module to websocket-client

--- websocket-client/src/test/java/sockets/SocketIoClientTest.java
+++ websocket-client/src/test/java/sockets/SocketIoClientTest.java
@@ -5,12 +5,6 @@ import com.pairapp.net.sockets.SocketIoClient;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
  * @author by Null-Pointer on 9/7/2015.
