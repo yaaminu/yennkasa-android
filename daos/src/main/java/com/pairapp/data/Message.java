7e675597784cfd9e9e9b56fbf60515ac599fa0ed
Null-Pointer <afolanyaaminu@gmail.com>
2016-11-19 07:55:03 +0000

    mark messages as seen even if it's a group message

--- daos/src/main/java/com/pairapp/data/Message.java
+++ daos/src/main/java/com/pairapp/data/Message.java
@@ -416,7 +416,7 @@ public class Message extends RealmObject {
         GenericUtils.ensureNotNull(realm, msgId);
         Message tmp = realm.where(Message.class).equalTo(Message.FIELD_ID, msgId).findFirst();
         if (tmp != null) {
-            if (tmp.getState() != Message.STATE_SEEN && !Message.isGroupMessage(tmp)) {
+            if (tmp.getState() != Message.STATE_SEEN) {
                 realm.beginTransaction();
                 if (tmp.isValid()) {
                     tmp.setState(Message.STATE_SEEN);
@@ -431,7 +431,7 @@ public class Message extends RealmObject {
         GenericUtils.ensureNotNull(realm, msgId);
         Message tmp = realm.where(Message.class).equalTo(Message.FIELD_ID, msgId).findFirst();
         if (tmp != null) {
-            if (tmp.getState() != STATE_SEEN && tmp.getState() != STATE_RECEIVED && !Message.isGroupMessage(tmp)) {
+            if (tmp.getState() != STATE_SEEN && tmp.getState() != STATE_RECEIVED) {
                 realm.beginTransaction();
                 if (tmp.isValid()) {
                     tmp.setState(STATE_RECEIVED);
